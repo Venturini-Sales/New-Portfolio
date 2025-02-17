32b280b9de8256ccdb4bec635422603a556194ba
João Pedro Venturini Sales <jpventurinisales@gmail.com>
2025-02-17 15:17:14 -0300

    Fix: Linkedin Link

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,96 +1,133 @@
-import './Home.css'
-import { useContext } from 'react';
-import { LanguageContext } from '../../contexts/context';
-import { textIdioms, miscellaneousIdioms } from '../../data/language';
-import discord from '../../assets/icons/discord.png'
-import github from '../../assets/icons/github.png'
-import linkedin from '../../assets/icons/linkedin.png'
-import whatsapp from '../../assets/icons/whatsapp.png'
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
-import { motion } from 'framer-motion';
+import "./Home.css";
+import { useContext } from "react";
+import { LanguageContext } from "../../contexts/context";
+import { textIdioms, miscellaneousIdioms } from "../../data/language";
+import discord from "../../assets/icons/discord.png";
+import github from "../../assets/icons/github.png";
+import linkedin from "../../assets/icons/linkedin.png";
+import whatsapp from "../../assets/icons/whatsapp.png";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 
 export const Home = () => {
+  const { language } = useContext(LanguageContext);
 
-    const { language } = useContext(LanguageContext);
+  const [text] = useTypewriter({
+    words: ["João Pedro Venturini Sales"],
+  });
 
-
-    const [text] = useTypewriter({
-        words: ['João Pedro Venturini Sales']
-    });
-
-    return (
-        <>
-        <div className='banner'>
-        <motion.div initial={{ x: '-20vw', opacity: 0 }} animate={{ x: 0, opacity: 1}} transition={{ type: 'spring', stiffness: 25}}>
-            <h1>{textIdioms.homeText[`${language}1`]}</h1>
+  return (
+    <>
+      <div className="banner">
+        <motion.div
+          initial={{ x: "-20vw", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 25 }}
+        >
+          <h1>{textIdioms.homeText[`${language}1`]}</h1>
         </motion.div>
 
-        <motion.div initial={{ x: '20vw', opacity: 0 }} animate={{ x: 0, opacity: 1}} transition={{ type: 'spring', stiffness: 25}}>
-            <h1>{textIdioms.homeText[`${language}2`]}</h1>
+        <motion.div
+          initial={{ x: "20vw", opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 25 }}
+        >
+          <h1>{textIdioms.homeText[`${language}2`]}</h1>
         </motion.div>
-            <h1 className='type'>
-                {text}
-                <Cursor cursorStyle='|'/>
-            </h1>
+        <h1 className="type">
+          {text}
+          <Cursor cursorStyle="|" />
+        </h1>
+      </div>
 
-        </div>
+      <div className="iconsArea">
+        <motion.div
+          initial={{ x: "-20vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", stiffness: 30 }}
+        >
+          <div className="flexIcons">
+            <a
+              target="blank"
+              href="https://www.linkedin.com/in/jo%C3%A3o-pedro-v-462600258/"
+            >
+              <div className="icons">
+                <img className="icon" src={linkedin} alt="icon" />
+              </div>
+            </a>
 
+            <a target="blank" href="https://github.com/Venturini-Sales">
+              <div className="icons">
+                <img className="icon" src={github} alt="icon" />
+              </div>
+            </a>
 
-        <div className='iconsArea'>
-        <motion.div initial={{ x: '-20vw' }} animate={{ x: 0}} transition={{ type: 'spring', stiffness: 30}}>
-        
-        <div className='flexIcons'>
-        <a target='blank' href="https://www.linkedin.com/in/jo%C3%A3o-pedro-venturini-sales-462600258/">
-            <div className='icons'>
-                <img className='icon' src={linkedin} alt="icon" />
-            </div>
-        </a>
+            <a
+              target="blank"
+              href="https://api.whatsapp.com/send?phone=5521978915683"
+            >
+              <div className="icons">
+                <img className="icon" src={whatsapp} alt="icon" />
+              </div>
+            </a>
 
-        <a target='blank' href="https://github.com/Venturini-Sales">
-            <div className='icons'>
-                <img className='icon' src={github} alt="icon" />
-            </div>  
-        </a>
-
-        <a target='blank' href="https://api.whatsapp.com/send?phone=5521978915683">
-            <div className='icons'>
-                <img className='icon' src={whatsapp} alt="icon" />
-            </div>
-        </a>
-
-        <a target='blank' href="https://discord.com/users/457338766521139203">
-            <div className='icons'>
-                <img className='icon' src={discord} alt="icon" />
-            </div>
-        </a>
-        </div>
+            <a
+              target="blank"
+              href="https://discord.com/users/457338766521139203"
+            >
+              <div className="icons">
+                <img className="icon" src={discord} alt="icon" />
+              </div>
+            </a>
+          </div>
         </motion.div>
-        </div>
+      </div>
 
-        <div className='scrollArea'>
-        <motion.div initial={{ x: '20vw' }} animate={{ x: 0}} transition={{ type: 'spring', stiffness: 30}}>
-        <div className='scroll'>
-        {miscellaneousIdioms.scrollDown[language]}
+      <div className="scrollArea">
+        <motion.div
+          initial={{ x: "20vw" }}
+          animate={{ x: 0 }}
+          transition={{ type: "spring", stiffness: 30 }}
+        >
+          <div className="scroll">
+            {miscellaneousIdioms.scrollDown[language]}
             <div className="c-infinite">
-                    <span className="arrowAnimation">
-                      <span className="animation">
-                        <span>
-                          <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.8333 13.9766L15.3033 9.50658L16.4816 10.6849L9.99998 17.1666L3.51831 10.6849L4.69664 9.50658L9.16664 13.9766V3.83325H10.8333V13.9766Z" fill="#ffffff"></path>
-                          </svg>
-                        </span>
-                        <span className="animatedArrow">
-                          <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.8333 13.9766L15.3033 9.50658L16.4816 10.6849L9.99998 17.1666L3.51831 10.6849L4.69664 9.50658L9.16664 13.9766V3.83325H10.8333V13.9766Z" fill="#ffffff"></path>
-                          </svg>
-                        </span>
-                      </span>
-                    </span>
-                  </div>
-        </div>
+              <span className="arrowAnimation">
+                <span className="animation">
+                  <span>
+                    <svg
+                      width="20"
+                      height="21"
+                      viewBox="0 0 20 21"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10.8333 13.9766L15.3033 9.50658L16.4816 10.6849L9.99998 17.1666L3.51831 10.6849L4.69664 9.50658L9.16664 13.9766V3.83325H10.8333V13.9766Z"
+                        fill="#ffffff"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span className="animatedArrow">
+                    <svg
+                      width="20"
+                      height="21"
+                      viewBox="0 0 20 21"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10.8333 13.9766L15.3033 9.50658L16.4816 10.6849L9.99998 17.1666L3.51831 10.6849L4.69664 9.50658L9.16664 13.9766V3.83325H10.8333V13.9766Z"
+                        fill="#ffffff"
+                      ></path>
+                    </svg>
+                  </span>
+                </span>
+              </span>
+            </div>
+          </div>
         </motion.div>
-        </div>
-        </>
-    )
-
-}
+      </div>
+    </>
+  );
+};
